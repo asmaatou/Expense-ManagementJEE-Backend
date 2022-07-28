712b876fcc416a8f549f71b7299c12a790b2a69b
Tounsi Asmaa <tasmaa2909@gmail.com>
2022-07-28 23:29:39 +0100

    2nd commit

--- src/main/java/com/stage/gestionnoteback/entities/Depense.java
+++ src/main/java/com/stage/gestionnoteback/entities/Depense.java
@@ -2,12 +2,14 @@ package com.stage.gestionnoteback.entities;
 
 import com.stage.gestionnoteback.enums.DepenseType;
 import com.stage.gestionnoteback.enums.InventionType;
+import javafx.scene.image.Image;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.awt.*;
 import java.util.Date;
 
 @Entity
@@ -24,6 +26,8 @@ public class Depense {
     private InventionType typeI;
     @Enumerated(EnumType.STRING)
     private DepenseType typeD;
+    @Column(nullable = true, length = 64)
+    private String photo;
     private Double montant;
 
 
