f178dd9834348e012b8c59ce95d43afa3d4e3ca9
Tounsi Asmaa <tasmaa2909@gmail.com>
2022-07-28 21:34:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#  Expense-ManagementJEE-Backend
+ 
---
+++ src/main/java/com/stage/gestionnoteback/GestionNoteBackApplication.java
@@ -0,0 +1,13 @@
+package com.stage.gestionnoteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNoteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNoteBackApplication.class, args);
+	}
+
+}
